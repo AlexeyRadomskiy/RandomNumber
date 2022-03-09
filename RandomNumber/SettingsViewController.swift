262c1a9a004b4dedf02749112bb47a6e0ac91194
Alexey <alexeyradomskiy98@gmail.com>
2022-03-09 19:47:45 +0300

    Release version 1.0.0 - done

---
+++ RandomNumber/SettingsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SettingsViewController.swift
+//  RandomNumber
+//
+//  Created by Alexey on 09.03.2022.
+//
+
+import UIKit
+
+class SettingsViewController: UIViewController {
+
+    @IBOutlet var minimumValueTF: UITextField!
+    @IBOutlet var maximumValueTF: UITextField!
+    
+    var minimumValue: String!
+    var maximumValue: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        minimumValueTF.text = minimumValue
+        maximumValueTF.text = maximumValue
+    }
+    
+    @IBAction func cancelButtonPressed() {
+        dismiss(animated: true)
+    }
+}
+
+
